03261b2b18a1f0df492561702d0cf30990454192
benfro <benfro@thermocalc.se>
2020-06-12 13:38:31 +0200

    Created Imaginary class

---
+++ src/main/java/net/benfro/tddedu/Imaginary.java
@@ -0,0 +1,12 @@
+package net.benfro.tddedu;
+
+public class Imaginary {
+
+   public final double re;
+   public final double im;
+
+   public Imaginary(double re, double im) {
+      this.re = re;
+      this.im = im;
+   }
+}
